79de5c1dc3049386b10727d32f330a8e627c6140
Ray Orsini <ray@orsiniit.com>
2020-05-09 22:01:43 -0400

    initial commit

---
+++ newcertificate.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+clear
+
+# Set variables
+account="e87af8285ab91c6d30ae453731febebc"
+certpath="/etc/apache2/sites-enabled/"
+
+
+printf "Let's get started registering a new cert.\n"
+read -p "Please enter the FQDN of the new cert: " fqdn
+
+## Format FQDN
+fqdn=$(printf $fqdn | tr "{A-Z}" "{a-z}")
+
+## Confirm intended FQDN
+read -p "You have entered $fqdn. Is that correct? [Y|N] " confirm
+confirm=$(printf $confirm | tr "{a-z}" "{A-Z}")
+
+## Checking if FQDN is confirmed
+if [ $confirm = Y ]
+then
+        # Call fqdn tests
+        printf "\nYou have confirmed $fqdn"
+
+        # Verify if there are any existing Apache configuration files with this name
+        printf "\nChecking for any existing configs for $fqdn..."
+
+        filename="${fqdn}.conf"
+        if test  -f $certpath$filename
+        then
+                printf  "\nConfiguration file already exists. Please pick a new FQDN. \nNow exiting...\n"
+                exit 1
+        else
+                printf "\nNo configuration found. Continuing certification...\n"
+        fi
+
+        # Create configuration file
+        sudo printf "<VirtualHost *:443>\nServerName $fqdn:443\nServerAlias $fqdn\nDocumentRoot /var/www/html/\n</VirtualHost>" > $ce$
+
+        # Verify new file was created
+        if test  -f $certpath$filename
+        then
+                printf  "\nNew configuration file created successfully. Beginning certification process...\n"
+                # Run Let's Encrypt with the new file
+                printf "\nRunning certbot for $fqdn....\n"
+                sudo certbot --apache --no-redirect --account $account -d $fqdn
+
+                printf "\nCertbot process complete. Please navigate to $fqdn to verify functionality.\n"
+        else
+                printf "\nUnable to create configuration file. Contact administrator. Exiting application.\n"
+                exit 1
+        fi
+else
+        printf "\nYou have not confirmed. Now exiting....\n"
+        exit 1
+fi
