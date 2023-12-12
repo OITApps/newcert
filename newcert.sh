a115cebc9c8ea5936f95b983fbd96f9051211ebc
sspaulding-oit <101204240+sspaulding-oit@users.noreply.github.com>
2023-12-12 14:08:16 -0700

    added menu option and second type of apache conf file for creation

--- newcert.sh
+++ newcert.sh
@@ -23,6 +23,13 @@ fac_sync_user="root"
 priv_key_loc="/home/$sync_user/.ssh/id_rsa"
 fac_priv_key_loc="/$fac_sync_user/.ssh/id_rsa"
 
+############################
+#Declaring Global Functions#
+############################
+
+#start of the portal fqdn generation function. 
+function createFqdn() {
+local sslSelection="$1"
 printf "Let's get started registering a new cert.\n"
 read -p "Please enter the FQDN of the new cert: " fqdn
 
@@ -51,18 +58,8 @@ then
                 printf "\nNo configuration found. Continuing certification...\n"
         fi
 
-        # Create configuration file
-        sudo cat <<- EOF > $certpath$filename
-                <VirtualHost *:443>
-                ServerName $fqdn:443
-                ServerAlias $fqdn
-                DocumentRoot /var/www/html/
-                AllowEncodedSlashes On
-                SSLCertificateFile /etc/letsencrypt/live/$fqdn/fullchain.pem
-                SSLCertificateKeyFile /etc/letsencrypt/live/$fqdn/privkey.pem
-                Include /etc/letsencrypt/options-ssl-apache.conf
-                </VirtualHost>
-EOF
+        # Create configuration file, the content of the conf file is determined by the case selection
+        eval $sslSelection
 
         # Verify new file was created
         if test  -f $certpath$filename
@@ -77,7 +74,51 @@ EOF
                 printf "\nUnable to create configuration file. Contact administrator. Exiting application.\n"
                 exit 1
         fi
+        fi
+        # Restart Apache
+        # Update Feb 22, 2023 - disabled reload of apache to prevent interruption of service.
+        # Auto apache reload is dependent on individual CRON jobs.  
+        printf "\nApache configuration will reload automatically at 4:00a EST nightly...\n"
+        #sudo service apache2 reload
+        printf "\nPeforming Apache Config Test..."
+        sudo apachectl configtest
+}
 
+function createPortal() {
+# Create configuration file
+        sudo cat <<- EOF > $certpath$filename
+<VirtualHost *:443>
+        ServerName $fqdn:443
+        ServerAlias $fqdn
+        DocumentRoot /var/www/html/
+        AllowEncodedSlashes On
+        SSLCertificateFile /etc/letsencrypt/live/$fqdn/fullchain.pem
+        SSLCertificateKeyFile /etc/letsencrypt/live/$fqdn/privkey.pem
+        Include /etc/letsencrypt/options-ssl-apache.conf
+</VirtualHost>
+EOF
+
+}
+function createDocs() {
+# Create configuration file
+        sudo cat <<- EOF > $certpath$filename
+<VirtualHost *:443>
+        ServerName $fqdn:443
+        ServerAlias $fqdn
+        ProxyPreserveHost Off
+        ProxyAddHeaders On
+        ProxyPass / https://voipdocs.helpjuice.com/ nocanon
+        ProxyPassReverse / https://voipdocs.helpjuice.com/
+        SSLCertificateFile /etc/letsencrypt/live/$fqdn/fullchain.pem
+        SSLCertificateKeyFile /etc/letsencrypt/live/$fqdn/privkey.pem
+        Include /etc/letsencrypt/options-ssl-apache.conf
+</VirtualHost>
+EOF
+
+}
+
+#Start of Rsyncing files to remote servers. 
+function syncCert() {
         #change file permissions to allow for rsyncing
         sudo chmod -R 0655 /etc/letsencrypt/keys/
         sudo chmod -R 0655 /etc/letsencrypt/archive/*/privkey* 
@@ -111,18 +152,71 @@ EOF
         else
             echo $(date) - "No hosted core servers to sync with." | tee -a $log_file
         fi
+}
 
-        # Restart Apache
-        # Update Feb 22, 2023 - disabled reload of apache to prevent interruption of service.
-        # Auto apache reload is dependent on individual CRON jobs.  
-        printf "\nApache configuration will reload automatically at 4:00a EST nightly...\n"
-        #sudo service apache2 reload
-        printf "\nPeforming Apache Config Test on local server..."
-        sudo apachectl configtest
-else
-        printf "\nYou have not confirmed. Now exiting....\n"
-        exit 1
-fi
+#Update another server after completion. 
+    function update_another() {
+                echo "Would you like to create another SSL? <Yes or No> "
+        read n
+            yes=$(echo n | tr -s '[:upper:]' '[:lower:]' || y)
+            if [[  "$n" == "yes" || "$n" == "y"  ]] ; then
+                clear
+            else
+                echo "Exiting Script! "
+                exit
+            fi
+}
+
+####################
+#start of SSL create selection
+####################
+#choose which SSL is to be created. 
+        while true; do
+        echo -e "Which SSL are you creating? \n"
+        echo -e "Please make your selection (<q> to quit)"
+        echo -e "1 - WLP Portal SSL"
+        echo -e "2 - WLP KB SSL"
+        echo -e "q - Quit\n"
+        echo "Enter Selection " 
+
+        read ssl
+        case $ssl in
+
+#generate portal apache site and SSL
+1)
+        #generate a portal SSL
+        createFqdn "createPortal"
+        echo -e "\n"
+        #rsync new files to remote servers
+        syncCert
+        echo -e "\n"       
+        #create another SSL
+        update_another 
+        ;;
+2)
+        #generate a docs SSL
+        createFqdn "createDocs"
+        echo -e "\n"
+        #rsync new files to remote servers
+        syncCert
+        echo -e "\n"
+        #create another SSL
+        update_another 
+        ;;
+q)
+#exit
+        echo "Exiting Script! "
+        exit
+        ;;
+*)
+#catch all response
+        echo "Please select a server "
+        ;;
+    
+        esac
+        done
+
+
 
 #TODO Function to create UTR or LE monitor
 #TODO Report status
